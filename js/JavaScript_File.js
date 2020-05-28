38d3f2b842bbea601a888eb82fd808ce804cd2ec
myronsinju <65003034+myronsinju@users.noreply.github.com>
2020-05-28 15:22:49 -0600

    Java repository

---
+++ js/JavaScript_File.js
@@ -0,0 +1,6 @@
+alert("Hello, World");
+var B = "whats up mane?";
+window.alert(B);
+
+var B = "Concatenated" + "string"
+document.write(B)
---
+++ js/basic_javascript.js
@@ -0,0 +1,18 @@
+window.alert("hellooooo, WORRRRRLDDDDD!");
+document.write('hello world');
+
+var blues = "I have the blues.";
+var blues = blues.fontcolor("blue");
+document.write(blues);
+
+document.write(3 + 3);
+
+function My_First_function()    {       //defining a function
+    var str = "This text is green!";    // giving a value to the string
+    var result = str.fontcolor("green"); //telling what color to call it
+    document.getElementById("Green_Text").innerHTML = result;   //showing the end result of the color by linking it to an ID
+}
+
+
+
+
