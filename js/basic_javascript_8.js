2e848708a0af4fc960cfd7ace66636c3a95077e0
myronsinju <65003034+myronsinju@users.noreply.github.com>
2020-06-02 00:53:13 -0600

    html 8 assignments
    html8

--- js/basic_javascript_8.js
+++ js/basic_javascript_8.js
@@ -6,3 +6,19 @@ function full_sentence()    {
     var whole_sentence = part_1.concat(part_2, part_3, part_4);
     document.getElementById("Concatenate").innerHTML = whole_sentence;
 }
+
+function slice_Method() {
+    var Sentence = "All work and no play makes Johnny a dull boy.";
+    var Section = Sentence.slice(27,33);
+    document.getElementById("Slice").innerHTML = Section;
+}
+
+function string_Method()    {
+    var X =182;
+    document.getElementById("Number_string").innerHTML = X.toString();
+}
+
+function precision_Method() {
+    var X = 12938.3012987376112;
+    document.getElementById("Precision").innerHTML = X.toPrecision(10);
+}
